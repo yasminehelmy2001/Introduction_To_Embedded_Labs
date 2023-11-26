117a7da5b220d26767f272cbf1be62bd1035ead4
yasminehelmy2001 <114500152+yasminehelmy2001@users.noreply.github.com>
2023-11-27 01:24:23 +0200

    task 1

---
+++ APP/Lab1/Task1/main.c
@@ -0,0 +1,21 @@
+#include "DIO.h"
+#include <util/delay.h>
+
+int main(void)
+{
+	DIO_enumSetPinConfig(DIO_enumPortA,DIO_enumPin0,DIO_enumOutput);
+	DIO_enumSetPinConfig(DIO_enumPortA,DIO_enumPin1,DIO_enumOutput);
+	
+	
+    while (1) 
+    {
+			//LED ON/OFF
+			DIO_enumSetPin(DIO_enumPortA,DIO_enumPin0,DIO_enumLogicHigh);
+			DIO_enumSetPin(DIO_enumPortA,DIO_enumPin1,DIO_enumLogicLow);
+			_delay_ms(1000);
+			DIO_enumSetPin(DIO_enumPortA,DIO_enumPin0,DIO_enumLogicLow);
+			DIO_enumSetPin(DIO_enumPortA,DIO_enumPin1,DIO_enumLogicHigh);
+			_delay_ms(1000);
+    }
+}
+
